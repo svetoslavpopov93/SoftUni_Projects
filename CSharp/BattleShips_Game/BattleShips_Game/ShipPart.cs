50129cb6f205eca611ad673ce44f9a4ab28a54bc
Svetoslav Popov <svetoslavpopov93@gmail.com>
2014-10-11 01:55:23 +0300

    ShipPart class

---
+++ CSharp/BattleShips_Game/BattleShips_Game/ShipPart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShips_Game
+{
+    class ShipPart
+    {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            set { }
+        }
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set { }
+        }
+
+        public ShipPart(int xValue, int yValue)
+        {
+            x = xValue;
+            y = yValue;
+        }
+    }
+}
